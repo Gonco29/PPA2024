4e29382a1227690f458790cc71265a7956f216e3
Gonco29 <goncobos@gmail.com>
2024-07-17 13:38:39 -0300

    Ya se agregan los productos al Carrito, desde index y show. Teniendo en cuenta si el precio está ON SALE.

---
+++ app/models/cart.rb
@@ -0,0 +1,9 @@
+class Cart < ApplicationRecord
+  belongs_to :user
+  has_many :cart_items, dependent: :destroy
+  has_many :products, through: :cart_items
+
+  def total_price
+    cart_items.to_a.sum { |item| item.item_price }
+  end
+end
