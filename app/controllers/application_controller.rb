0346c299e2f1bb19f7e148a6614d2de1b4881497
Gonco29 <goncobos@gmail.com>
2024-07-27 19:22:32 -0300

    prueba error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit
+
+  before_action :set_cart_items_count
+
+  private
+
+  def set_cart_items_count
+    if user_signed_in?
+      @cart_items_count = current_user.cart&.cart_items&.sum(:quantity) || 0
+    else
+      @cart_items_count = 0
+    end
+  end
+
 end
