7703be1477ed89737f06a60651b67f81386361e7
Gonco29 <goncobos@gmail.com>
2024-12-05 18:24:34 -0300

    Pagina sin necesidad de Usuario

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  # skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
     @random_products = Product.order("RANDOM()").limit(5)
