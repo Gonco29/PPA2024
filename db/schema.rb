120a7418dc99b1e8a82c089bd350e7b8d9286309
Gonco29 <goncobos@gmail.com>
2025-01-04 17:06:03 -0300

    Logica de Orders, actualizada con nuevos datos y funcional. Falta mail

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_19_154501) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_04_180810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +75,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_19_154501) do
     t.decimal "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "full_name"
+    t.string "email"
+    t.string "phone"
+    t.string "rut"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
