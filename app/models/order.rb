0a231e07ba6f41b750a219aef240c81229f530ab
Gonco29 <goncobos@gmail.com>
2025-01-05 16:15:23 -0300

    Terminos y condiciones asociados al form, fracasé con el boton desabilitado y el modal aparece solo una vez al principio

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,7 @@ class Order < ApplicationRecord
 
   validates :full_name, :email, :phone, :rut, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "no es válido" }
+  validates :terms_and_conditions, acceptance: true
 
   # validates :phone, numericality: { only_integer: true, message: "debe contener solo números" }, length: { in: 8..15 }
   # validate :valid_rut_format
